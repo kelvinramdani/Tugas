6891b5fdcbabd7930741d1794103bf72c6b0b4f7
Kelpin Ramdani <kelvinramdani9851@gmail.com>
2025-09-20 20:18:09 +0700

    Add files via upload

---
+++ statement elif.py
@@ -0,0 +1,10 @@
+tinggi = input("berapa tinggi badan kamu?")
+
+if tinggi=="160" :
+  print ("kamu cukup")
+elif tinggi=="161" :
+  print ("kamu cukup")
+elif tinggi=="162" :
+  print ("kamu sangat ideal")
+else:
+  print ("kamu aneh")
