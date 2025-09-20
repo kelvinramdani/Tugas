e84e1741af7be713b5ed71d261a76508476bfac1
Kelpin Ramdani <kelvinramdani9851@gmail.com>
2025-09-20 20:16:32 +0700

    Add files via upload

---
+++ operasi aritmatika.py
@@ -0,0 +1,95 @@
+#operasi aritmatika 
+
+a = 10
+b = 3
+
+#operasi tambah +
+hasil = a + b
+print(a,'+',b,'=',hasil)
+  
+#operasi kurang -
+hasil = a - b
+print(a,'-',b,'=',hasil)
+
+#operasi kalian *
+hasil = a * b
+print(a,'*',b,'=',hasil)
+
+#operasi bagi /
+hasil = a / b
+print(a,'/',b,'=',hasil)
+
+#operasi sisa bagi %
+hasil = a % b
+print(a,'%',b,'=',hasil)
+
+#operasi Pemangkatan **
+hasil = a ** b
+print(a,'**',b,'=',hasil)
+
+#operator sama dengan ==
+hasil = a == b
+print(a,'==',b,'=',hasil)
+
+#operator tidak sama dengan !=
+hasil = a != b
+print(a,'!=',b,'=',hasil)
+
+#operator lebih besar >
+hasil = a > b
+print(a,'>',b,'=',hasil)
+
+
+#operator lebih kecil <
+hasil = a < b
+print(a,'<',b,'=',hasil)
+
+
+#operator lebih besar atau sama dengan >=
+hasil = a >= b
+print(a,'>=',b,'=',hasil)
+
+#operator lebih kecil atu sama dengan <=
+hasil = a <= b
+print(a,'<=',b,'=',hasil)
+
+#operator logika AND and
+hasil = a and b
+print(a,'and',b,'=',hasil)
+
+
+#operator logika OR or
+hasil = a or b
+print(a,'or',b,'=',hasil)
+
+
+#operator negasi/kebalikan not
+hasil =  not a
+print(a,'not',b,'=',hasil)
+
+#operator Bitwise and %
+hasil = a % b
+print(a,'%',b,'=',hasil)
+
+#operator Bitwise or |
+hasil = a | b
+print(a,'|',b,'=',hasil)
+
+
+#operator Bitwise Xor ^
+hasil = a ^ b
+print(a,'^',b,'=',hasil)
+
+
+#operator Bitwise i's complement ~
+hasil =  ~ b
+print(a,'~',b,'=',hasil)
+
+
+#operator Bitwise left-shift <<
+hasil = a << b
+print(a,'<<',b,'=',hasil)
+
+#operator Bitwise right-shift >>
+hasil = a >> b
+print(a,'>>',b,'=',hasil)
