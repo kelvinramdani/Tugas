20a1de96efbc5124ac0740ce957625918e410322
Kelpin Ramdani <kelvinramdani9851@gmail.com>
2025-09-20 20:18:52 +0700

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,14 @@
+nama = input("kamu siapa?")
+
+#if kondisi:aksi
+
+#if nama=="kelpin" : print("kamu manis sekal!!!")
+#print(f"terima kasih {nama}")
+
+if nama=="Kelpin" :
+  print("kamu ganteng abieeez!")
+  print("kamu juga keren banget!")
+  print("kamu hebat bangetttt")
+  print("mantap")
+  print(f"Terima kasih {nama}")
+  
