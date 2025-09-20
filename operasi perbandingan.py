f6fd65e49af6f35375337e363b95a518bda4fb7a
Kelpin Ramdani <kelvinramdani9851@gmail.com>
2025-09-20 20:16:15 +0700

    Add files via upload

---
+++ operasi perbandingan.py
@@ -0,0 +1,25 @@
+#operator sama dengan ==
+hasil = a == b
+print(a,'==',b,'=',hasil)
+
+#operator tidak sama dengan !=
+hasil = a != b
+print(a,'!=',b,'=',hasil)
+
+#operator lebih besar >
+hasil = a > b
+print(a,'>',b,'=',hasil)
+
+
+#operator lebih kecil <
+hasil = a < b
+print(a,'<',b,'=',hasil)
+
+
+#operator lebih besar atau sama dengan >=
+hasil = a >= b
+print(a,'>=',b,'=',hasil)
+
+#operator lebih kecil atu sama dengan <=
+hasil = a <= b
+print(a,'<=',b,'=',hasil)
