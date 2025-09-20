ae21811dac5a144f9784c94d2a267b340b21ee14
Kelpin Ramdani <kelvinramdani9851@gmail.com>
2025-09-20 20:19:37 +0700

    Add files via upload

---
+++ nama.py
@@ -0,0 +1,6 @@
+#python 3.7.1
+print ("no absen         = 17")
+print ("nama             = Kelpin")
+print ("kelas            = 11 rpl 1")
+print ("mata pelajaran   = PTGM")
+print ("alamat           = kp.Cisaradan girang")
